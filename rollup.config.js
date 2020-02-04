c03b1d045ecfffddfb6ad66d8fd0d6360ca5e53a
obedm503 <obedm503@gmail.com>
2020-02-04 12:10:07 -0600

    basic rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,51 @@
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
-/** @type {import('rollup').RollupOptions} */
-const config = {
-  input: 'src/asciimath.ts',
-  plugins: [typescript(), resolve()],
-  output: [
-    {
-      file: 'dist/asciimath.cjs',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/asciimath.mjs',
-      format: 'esm',
-    },
-    {
-      file: 'dist/asciimath.js',
-      format: 'umd',
-      name: 'asciimath',
-    },
-  ],
-};
-export default config;
+/**
+ * @param {import('rollup').RollupOptions} [override]
+ * @returns {import('rollup').RollupOptions}
+ */
+function config(override = {}) {
+  return {
+    input: 'src/asciimath.ts',
+    external: source => /lodash/.test(source),
+    ...override,
+  };
+}
+
+export default [
+  // node builds
+  config({
+    plugins: [resolve(), commonjs(), typescript({ module: 'ESNext' })],
+    output: [
+      {
+        file: 'dist/asciimath.cjs',
+        format: 'commonjs',
+      },
+      {
+        file: 'dist/asciimath.mjs',
+        format: 'module',
+      },
+    ],
+  }),
+  // browser builds
+  // TODO: browser bundles still include all of lodash!
+  config({
+    external: [],
+    output: [
+      {
+        name: 'asciimath',
+        file: 'dist/asciimath.js',
+        format: 'iife',
+      },
+      {
+        name: 'asciimath',
+        file: 'dist/asciimath.min.js',
+        format: 'iife',
+        plugins: [terser()], // minifies
+      },
+    ],
+  }),
+];
